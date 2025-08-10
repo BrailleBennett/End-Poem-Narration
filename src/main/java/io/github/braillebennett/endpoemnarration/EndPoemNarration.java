28d756749795ec2a01421541c51e4654aa456f45
BrailleBennett <103331457+BrailleBennett@users.noreply.github.com>
2025-08-09 22:19:13 -0500

    Use project Lombok for logging

--- src/main/java/io/github/braillebennett/endpoemnarration/EndPoemNarration.java
+++ src/main/java/io/github/braillebennett/endpoemnarration/EndPoemNarration.java
@@ -1,22 +1,21 @@
 package io.github.braillebennett.endpoemnarration;
 
+import lombok.extern.slf4j.Slf4j;
 import net.fabricmc.api.ClientModInitializer;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
+@Slf4j
 public class EndPoemNarration implements ClientModInitializer {
     public static final String MOD_ID = "end-poem-narration";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     public static final ResourceLocation POEM_NARRATION = ResourceLocation.parse("end-poem-narration:poem-narration");
     public static SoundEvent POEM_NARRATION_SOUND_EVENT = SoundEvent.createVariableRangeEvent(POEM_NARRATION);
 
     @Override
     public void onInitializeClient() {
-        LOGGER.info("Registering End Poem Narration sound.");
+        log.info("Registering End Poem Narration sound.");
         Registry.register(BuiltInRegistries.SOUND_EVENT, EndPoemNarration.POEM_NARRATION, POEM_NARRATION_SOUND_EVENT);
     }
 }
