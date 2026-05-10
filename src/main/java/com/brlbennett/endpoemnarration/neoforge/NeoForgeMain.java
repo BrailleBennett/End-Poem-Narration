43bc96b39b6d837c45dda445a8691830c6ebcb9b
BrailleBennett <103331457+BrailleBennett@users.noreply.github.com>
2026-05-10 04:26:19 -0500

    Full Neo support

--- src/main/java/com/brlbennett/endpoemnarration/neoforge/NeoForgeMain.java
+++ src/main/java/com/brlbennett/endpoemnarration/neoforge/NeoForgeMain.java
@@ -2,11 +2,13 @@ package com.brlbennett.endpoemnarration.neoforge;
 
 import com.brlbennett.endpoemnarration.EndPoemNarration;
 import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.registries.DeferredRegister;
 
 @Mod(value = EndPoemNarration.MOD_ID, dist = Dist.CLIENT)
 public class NeoForgeMain {
-    public NeoForgeMain() {
-        EndPoemNarration.initialize();
+    public NeoForgeMain(IEventBus modBus) {
+        PoemNarrationSound.SOUND_EVENTS.register(modBus);
     }
 }
---
+++ src/main/java/com/brlbennett/endpoemnarration/neoforge/PoemNarrationSound.java
@@ -0,0 +1,12 @@
+package com.brlbennett.endpoemnarration.neoforge;
+
+import com.brlbennett.endpoemnarration.EndPoemNarration;
+import net.minecraft.core.Holder;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.sounds.SoundEvent;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class PoemNarrationSound {
+    public static final DeferredRegister<SoundEvent> SOUND_EVENTS = DeferredRegister.create(BuiltInRegistries.SOUND_EVENT, EndPoemNarration.MOD_ID);
+    public static final Holder<SoundEvent> MY_SOUND = SOUND_EVENTS.register("poem_narration", SoundEvent::createVariableRangeEvent);
+}
